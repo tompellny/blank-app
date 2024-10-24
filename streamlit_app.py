ab415954dac4e38bc6343751dc53a4e3d4821d9e
Tom Pellny <101478675+tompellny@users.noreply.github.com>
2024-10-24 23:10:56 +0000

    text chg

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,8 @@ import pandas as pd
 st.set_page_config(page_title="InsideOptions — Upload", layout="wide")
 
 # ---------------- STATIC PAGE DESCRIPTION ----------------
-st.title('Data Upload')
-#st.image("assets/logo_bull_bear.png", width=200)
-st.image("assets/logo_finstory_symboltext.png", width=150)
-
+st.image("assets/logo_finstory_symboltext.png", width=100)
+st.title('FinStory «Streamlit»')
 
 st.subheader("Upload XY", divider="red")
 st.write("Upload section here")
