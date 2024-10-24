42b8aa86557dcecb131270f85e6f9b339b742dc5
Tom Pellny <101478675+tompellny@users.noreply.github.com>
2024-10-24 22:51:24 +0000

    renamed main file

--- Home.py
+++ Home.py
@@ -7,6 +7,8 @@ st.set_page_config(page_title="InsideOptions — Upload", layout="wide")
 # ---------------- STATIC PAGE DESCRIPTION ----------------
 st.title('Data Upload')
 st.image("assets/logo_bull_bear.png", width=200)
+#st.image("assets/logo_finstory_animated.gif", width=200)
+
 
 st.subheader("Upload XY", divider="red")
 st.write("Upload section here")
