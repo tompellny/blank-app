40802bf8ea18813e0f435ecfa69b611d0c3b9268
Tom Pellny <101478675+tompellny@users.noreply.github.com>
2024-10-24 23:09:14 +0000

    pic change

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.set_page_config(page_title="InsideOptions — Upload", layout="wide")
 
 # ---------------- STATIC PAGE DESCRIPTION ----------------
 st.title('Data Upload')
-st.image("assets/logo_bull_bear.png", width=200)
-st.image("assets/logo_finstory_symboltext.png", width=200)
+#st.image("assets/logo_bull_bear.png", width=200)
+st.image("assets/logo_finstory_symboltext.png", width=150)
 
 
 st.subheader("Upload XY", divider="red")
